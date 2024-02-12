ae2dd60b860db5ad42aee6486a9764c2ba5efff3
Soumen Gope <124480121+soumengope@users.noreply.github.com>
2024-02-12 08:52:31 +0530

    Update page.js
    added alt with empty value inside Image component

--- app/page.js
+++ app/page.js
@@ -12,15 +12,15 @@ export default function Home() {
   return (
     <div className={styles.mainDiv}>
 
-      <div className={styles.imgLogo}><Image src="/logo.svg" width={42.984} height={67.546}></Image></div>
+      <div className={styles.imgLogo}><Image src="/logo.svg" width={42.984} height={67.546} alt="" ></Image></div>
       
       <div className={styles.top_mainSection}>
         <div className={styles.head_desc}>
           <div className={styles.header}>Registration</div>
           <div className={styles.description}>Onboarding Design</div>
         </div>
-        <div className={styles.top_imgdiv1}><Image src="/1.png" width={450.379} height={308.478}></Image></div>
-        <div className={styles.top_imgdiv2}><Image src="/3.png" width={87} height={308.478}></Image></div>
+        <div className={styles.top_imgdiv1}><Image src="/1.png" width={450.379} height={308.478}alt="" ></Image></div>
+        <div className={styles.top_imgdiv2}><Image src="/3.png" width={87} height={308.478}alt=""></Image></div>
       </div>
 
       <div className={styles.bot_mainSection}>
@@ -28,8 +28,8 @@ export default function Home() {
           <div className={styles.bot_header}>Dribbble</div>
           <div className={styles.bot_description}>@Presh_legacy</div>
         </div>
-        <div className={styles.bot_imgdiv1}><Image src="/41.png" width={450.379} height={250.478}></Image></div>
-        <div className={styles.bot_imgdiv2}><Image src="/2.png" width={187.379} height={250.478}></Image></div>
+        <div className={styles.bot_imgdiv1}><Image src="/41.png" width={450.379} height={250.478} alt=""></Image></div>
+        <div className={styles.bot_imgdiv2}><Image src="/2.png" width={187.379} height={250.478} alt=""></Image></div>
       </div>
 
     </div>
