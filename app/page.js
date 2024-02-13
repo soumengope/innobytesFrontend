d00a2955180f05fe74dd6e39156e9a3af131ca24
Soumen Gope <124480121+soumengope@users.noreply.github.com>
2024-02-13 05:41:42 +0530

    Update page.js
    redirect

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 export default function Home() {
   useEffect(()=>{
     const redirectPage = setTimeout(()=>{
-      window.location.href="https://innobytes-frontend.vercel.app/myUser";
+      window.location.href="https://innobytes-frontend-woad.vercel.app/myUser";
     },1000)
   },[])
   return (
